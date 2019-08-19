af34edf5de83207448ec7c6f66ec68a70d5fb711
Cristina Souza <cristina.carvalho.b@gmail.com>
2019-08-19 10:56:54 -0700

    Add up to sessions

---
+++ biddr_api/biddr_api/app/controllers/api/v1/bids_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::BidsController < Api::ApplicationController
+    before_action :set_auction
+    before_action :set_bid, only: [:show]
+    
+    def index
+        render json: @auction.bids
+    end
+
+    def create
+        @auction.bids.create(bid_params)
+        render json: @auction.bids.last
+    end
+
+    def show
+        render json: @auction
+    end
+
+    private
+    def bid_params
+        params.require(:bid).permit(:price)
+    end
+
+    def set_auction
+        @auction = Auction.find(params[:auction_id])
+    end
+
+    def set_course
+        @bid = @auction.bids.find_by!(id: params[:id])
+    end
+end
---
+++ biddr_api/biddr_api/db/migrate/20190819165138_create_auctions.rb
@@ -0,0 +1,13 @@
+class CreateAuctions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :auctions do |t|
+      t.string :title
+      t.string :description
+      t.decimal :price
+      t.date :ends_at
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
